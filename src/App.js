d3aee623a0b3a12f416575cc086e7abee1db44d9
nicanor-alicaya <donalicaya@gmail.com>
2019-11-11 12:08:01 -0500

    MVP achieved

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import { Route } from'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path = '/'></Route>
-    <Route path = '/about'></Route>
-    <Route path = '/contact'></Route>
-      
-    <Route/>
+<Route exact path = '/'>{Home}</Route>
+    <Route path = '/about'>{About}</Route>
+    <Route path = '/contact'>{Contact}</Route> 
   </div>
 );
 
